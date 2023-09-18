7cb407599a21866c0e2bcee991791ac1a855c7e5
ludovicobesana <ludovicobesanaguide@gmail.com>
2023-09-18 18:58:48 +0200

    fix: Fixed meta description

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Bootcamp JavaScript by SheTech e Deloitte">
+      description="Python Starter Kit by SheTech e UniCredit">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
