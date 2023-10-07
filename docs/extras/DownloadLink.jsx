228dc7d018edf3fff9bc7981bfabeeb630dac79b
Ludovico Besana <35035423+ludovicobesana@users.noreply.github.com>
2023-10-07 15:42:01 +0000

    feat: Add solutions and refactoring download link

--- docs/extras/DownloadLink.jsx
+++ docs/extras/DownloadLink.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import useBaseUrl from '@docusaurus/useBaseUrl'
 
 const DownloadLink = ({ label }) => {
-  const csvUrl = useBaseUrl('dataset.csv')
+  const csvUrl = useBaseUrl('soluzioni-progetto-finale.ipynb')
   return <a target="_blank" rel="noopener noreferrer" href={csvUrl} download>{label}</a>
 }
 
